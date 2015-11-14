cb7928a6f0e56c65485998a5303c85413852b5a4
sanketmarkan <you@example.com>
2015-11-15 05:09:04 +0530

    c6

--- rateguru/main/forms.py
+++ rateguru/main/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class FeedbackForm(forms.Form):
-	#Options = (1,2,3,4,5)
-	q1 = forms.IntegerField(required=True)
-	q2 = forms.IntegerField(required=True)
-
+	MY_CHOICES = ((1, '1'),(2, '2'),(3, '3'),(4, '4'),(5, '5'),)
+	q1 = forms.ChoiceField(widget=forms.RadioSelect, choices=MY_CHOICES,required=True, label='Q1 text')
+	q2 = forms.ChoiceField(widget=forms.RadioSelect, choices=MY_CHOICES,required=True, label='Q2 text')
+  
