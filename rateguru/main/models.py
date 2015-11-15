51d92bbc9404907fa0985d0048d17cd8779bc575
hemantkasat <hemantkasat@gmail.com>
2015-11-15 08:43:31 +0530

    first commit

--- rateguru/main/models.py
+++ rateguru/main/models.py
@@ -19,7 +19,9 @@ class Comments(models.Model):
 	prof_id = models.ForeignKey(Prof)
 	description = models.CharField(max_length=200)
 	helpfulness = models.IntegerField()
-
+class Courses(models.Model):
+	Name = models.CharField()
+	semester = models.IntegerField()
 #class FeedbackForm(ModelForm):
 #	 name = forms.CharField(max_length=100)
 #	 title = forms.CharField(max_length=3,widget=forms.Select(choices=TITLE_CHOICES))
