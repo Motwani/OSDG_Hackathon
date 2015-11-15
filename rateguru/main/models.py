9613219581d9f74b3a6a745240f0ff550a512d1b
hemantkasat <hemantkasat@gmail.com>
2015-11-15 09:05:39 +0530

    second commit

--- rateguru/main/models.py
+++ rateguru/main/models.py
@@ -2,26 +2,24 @@ from django.db import models
 #from django.forms import ModelForm
 from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+class Courses(models.Model):
+	Name = models.CharField(max_length=20)
+	semester = models.IntegerField()
 class Prof(models.Model):
 	prof_name = models.CharField(max_length=200)
-	courses = models.ManyToManyField(Courses)
+	courses = models.ManyToManyField(Courses)	
 class Rating(models.Model):
 	prof_id = models.ForeignKey(Prof)
 	clarity = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
 	helpfullness = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
 	friendly = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
 	dedicated = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
-
 class Student(models.Model):
 	name = models.CharField(max_length=200)
-
 class Comments(models.Model):
 	prof_id = models.ForeignKey(Prof)
 	description = models.CharField(max_length=200)
 	helpfulness = models.IntegerField()
-class Courses(models.Model):
-	Name = models.CharField()
-	semester = models.IntegerField()
 #class FeedbackForm(ModelForm):
 #	 name = forms.CharField(max_length=100)
 #	 title = forms.CharField(max_length=3,widget=forms.Select(choices=TITLE_CHOICES))
